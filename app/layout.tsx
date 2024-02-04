e86ed3759f25f2c1d025a1d3da4b09ce62a97ee0
Taiwo Jolomi <taiwojolomi15@gmail.com>
2024-02-04 02:53:36 +0100

    Initial commit from Create Next App

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Nunito } from "next/font/google";
+import "./globals.css";
+
+const font = Nunito({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "GasGnG",
+  description: "Destinations at your convienence",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={font.className}>{children}</body>
+    </html>
+  );
+}
