6edfc83cfc80bae9f3bc41c8e44562990c01af8e
Taiwo Jolomi <taiwojolomi15@gmail.com>
2024-03-14 04:32:42 +0100

    - implemented google signin

--- app/components/modals/LoginModal.tsx
+++ app/components/modals/LoginModal.tsx
@@ -80,7 +80,7 @@ const LoginModal = () => {
 				outline
 				label="Continue with Google"
 				icon={FcGoogle}
-				onClick={() => {}}
+				onClick={() => signIn("google")}
 			/>
 			<Button
 				outline
